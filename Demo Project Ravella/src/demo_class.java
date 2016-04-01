22d1b2e04a7e723c169461890f29f75c708dc1eb
sravella <Srini Cyber@SriniCyber-PC.attlocal.net>
2016-04-01 18:31:03 -0500

    add

---
+++ Demo Project Ravella/src/demo_class.java
@@ -0,0 +1,10 @@
+
+public class demo_class {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Ravella Sample java calss code	
+		System.out.println("Koti is the java guru");
+	}
+
+}
